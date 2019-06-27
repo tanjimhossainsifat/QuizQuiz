3c0bcfb13cbd6f02e734f2328d0a791e803eb9f7
Tanjim Hossain <tanjimhossainsifat@gmail.com>
2019-06-27 19:00:06 +0600

    UI and Controller created for QuizView

---
+++ QuizQuiz/QuizQuiz/Controller/QuizViewController.swift
@@ -0,0 +1,45 @@
+//
+//  QuizViewController.swift
+//  QuizQuiz
+//
+//  Created by Tanjim Hossain Sifat on 27/6/19.
+//  Copyright © 2019 Tanjim Hossain Sifat. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var buttonA: UIButton!
+    @IBOutlet weak var buttonB: UIButton!
+    @IBOutlet weak var buttonC: UIButton!
+    @IBOutlet weak var buttonD: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+
+    @IBAction func onButtonA(_ sender: UIButton) {
+    }
+    
+    @IBAction func onButtonB(_ sender: UIButton) {
+    }
+    
+    @IBAction func onButtonC(_ sender: UIButton) {
+    }
+    
+    @IBAction func onButtonD(_ sender: UIButton) {
+    }
+    
+    @IBAction func onButtonNext(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
